6c7998fc2daa2ee10b61e4428ef785eda7ff4267
zengxu <1824677900@qq.com>
2023-03-23 15:13:59 +0800

    1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GptController } from './gpt/gpt.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, GptController],
   providers: [AppService],
 })
 export class AppModule {}
